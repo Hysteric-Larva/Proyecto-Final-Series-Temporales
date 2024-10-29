03903069773a8fd6d7c453159ade8f9e72b752bb
Hysteric <44246648+Hysteric-Larva@users.noreply.github.com>
2024-10-28 21:09:30 -0300

    Add files via upload

--- Proyecto Final.r
+++ Proyecto Final.r
@@ -1,7 +1,62 @@
-file_path <- "C:\\Users\\kzep\\Tareas Series Temporales\\Proyecto Final\\data\\GNP.csv"
+dev.new()
+library(astsa)
+library(quantmod)
+library(forecast)
+library(ggplot2)
+library(tseries)
 
-# Read the CSV file into a data frame
-gnp <- read.csv(file_path)
 
+#A <-getSymbols("INTC", src = "yahoo", from = "2024-01-01", to = "2024-07-01")
+#B <-getSymbols("INTC", src = "yahoo", from = "2024-01-01", to = "2024-08-15")
+#C <- getSymbols("INTC", src = "yahoo", from = "2024-01-01", to = "2024-10-24")
+#getSymbols("INTC", src = "yahoo", from = "2024-06-01", to = "2024-8-08")#despues caida
+getSymbols("INTC", src = "yahoo", from = "2024-03-01", to = "2024-6-30")
 
-print(gnp)
+chartSeries(INTC)  # Plot the stock price chart for Apple
+
+#getSymbols("NVDA", src = "yahoo", from = "2024-04-01", to = "2024-10-01")
+#chartSeries(NVDA)
+
+values <- INTC[,6]
+fit <- auto.arima(values)
+summary(fit)
+#plot(forecast(fit,h=10))
+
+resultado_adf <- adf.test(log(values))
+print(resultado_adf)
+
+###########################
+# PRIMRO VEAMOS LAMBDA POSITIVOS
+###########################
+lambda_list <- list()
+p_list <- list()
+for(i in 1:110){
+  lambda <- i
+  lambda_list[i]<- i
+  values_2 <- (values^i-1)/i
+  
+  test <- adf.test(values_2)
+  p_list[i] <- test$p.value
+}
+
+plot(lambda_list,p_list)
+abline(h=0.05, col="red", lty=2) 
+
+#############################
+# Lambda negaticos
+##############################
+
+lambda_list <- list()
+p_list <- list()
+for(i in 1:110){
+  lambda <- -i
+  lambda_list[i]<- i
+  values_2 <- (values^lambda-1)/lambda
+  
+  test <- adf.test(values_2)
+  p_list[i] <- test$p.value
+}
+
+plot(lambda_list,p_list)
+abline(h=0.05, col="red", lty=2) 
+
